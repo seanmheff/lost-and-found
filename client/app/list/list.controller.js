7c453ff8d5acbc5e9259c67915d7fa45342f2d95
Sean Heffernan <seanmheff@gmail.com>
2014-11-16 01:55:42 -0500

    Add some item view styling

--- client/app/list/list.controller.js
+++ client/app/list/list.controller.js
@@ -1,6 +1,39 @@
 'use strict';
 
+var itemIconClasses = {
+  'electronics': 'glyphicon glyphicon-phone',
+  'misc': 'glyphicon glyphicon-star-empty',
+  'personal': 'glyphicon glyphicon-briefcase',
+};
+
 angular.module('lostAndFoundApp')
   .controller('ListCtrl', function ($scope) {
     $scope.message = 'Hello';
+
+    $scope.items = [
+      {
+        heading: "Bag of weed found on Glann Road.",
+        type: "misc",
+        time: new Date()
+      },
+      {
+        heading: "Wallet found in Eye Cinema",
+        type: "personal",
+        time: new Date()
+      },
+      {
+        heading: "Nokia 3510i found at Tesco.",
+        type: "electronics",
+        time: new Date()
+      },
+    ];
+
+    /**
+     * A function that gets an items icon CSS class.
+     * @param {string} itemType - The item type.
+     * @return {string}
+     */
+    $scope.getIconClass = function(itemType) {
+      return itemIconClasses[itemType];
+    };
   });
