4f6419f3ef297421d1a4bfe33a4de7c3362e34a8
Sean Heffernan <seanmheff@gmail.com>
2014-12-04 17:03:04 -0500

    Update main map view

--- client/app/map/map.controller.js
+++ client/app/map/map.controller.js
@@ -1,14 +1,43 @@
 'use strict';
 
 angular.module('lostAndFoundApp')
-  .controller('MapCtrl', function ($scope) {
-
+  .controller('MapCtrl', function ($scope, $http, uiGmapGoogleMapApi) {
+    // var bounds = new google.maps.LatLngBounds();
     $scope.map = {
-	    center: {
+      center: {
         latitude: 40.74349,
         longitude: -73.990822
-	    },
-	    zoom: 12
-		};
-      
+      },
+      zoom: 12,
+      dragging: false,
+      bounds: {}
+    };
+
+    uiGmapGoogleMapApi.then(function(maps) {
+      $http.get('/api/items/').success(function(items) {
+        var markers = [];
+        var bounds = new google.maps.LatLngBounds();
+
+        angular.forEach(items, function(item) {
+          bounds.extend(new google.maps.LatLng(item.location.latitude, item.location.longitude));
+          this.push(item.location);
+        }, markers);
+
+        $scope.map.bounds = {
+          northeast: {
+            latitude: bounds.getNorthEast().lat() + 0.005,
+            longitude: bounds.getNorthEast().lng() + 0.005
+          },
+          southwest: {
+            latitude: bounds.getSouthWest().lat() - 0.005,
+            longitude: bounds.getSouthWest().lng() - 0.005
+          }
+        };
+
+        $scope.map.markers = markers;
+      });
+    });
+
+
+
   });
