e20865b401d51f7b590e51a4d3f06a7021253d9a
Sean Heffernan <seanmheff@gmail.com>
2014-12-06 12:38:05 -0500

    Add client geolocation support

--- client/app/map/map.controller.js
+++ client/app/map/map.controller.js
@@ -1,8 +1,9 @@
 'use strict';
 
 angular.module('lostAndFoundApp')
-  .controller('MapCtrl', function ($scope, $http, uiGmapGoogleMapApi) {
-    // var bounds = new google.maps.LatLngBounds();
+  .controller('MapCtrl', function ($scope, $http, uiGmapGoogleMapApi, Location, $rootScope) {
+
+    $scope.mapLoaded = false;
     $scope.map = {
       center: {
         latitude: 40.74349,
@@ -14,28 +15,45 @@ angular.module('lostAndFoundApp')
     };
 
     uiGmapGoogleMapApi.then(function(maps) {
-      $http.get('/api/items/').success(function(items) {
-        var markers = [];
-        var bounds = new google.maps.LatLngBounds();
+      // // Get all the items
+      // $http.get('/api/items/').success(function(items) {
+      //   var markers = [];
+      //   var bounds = new google.maps.LatLngBounds();
 
-        angular.forEach(items, function(item) {
-          bounds.extend(new google.maps.LatLng(item.location.latitude, item.location.longitude));
-          this.push(item.location);
-        }, markers);
+      //   // For each item, create a marker, and extend the maps bounds
+      //   angular.forEach(items, function(item) {
+      //     bounds.extend(new google.maps.LatLng(item.location.latitude, item.location.longitude));
+      //     this.push(item.location);
+      //   }, markers);
 
-        $scope.map.bounds = {
-          northeast: {
-            latitude: bounds.getNorthEast().lat() + 0.005,
-            longitude: bounds.getNorthEast().lng() + 0.005
-          },
-          southwest: {
-            latitude: bounds.getSouthWest().lat() - 0.005,
-            longitude: bounds.getSouthWest().lng() - 0.005
-          }
-        };
+      //   // Set the map bounds
+      //   $scope.map.bounds = {
+      //     northeast: {
+      //       latitude: bounds.getNorthEast().lat() + 0.005,
+      //       longitude: bounds.getNorthEast().lng() + 0.005
+      //     },
+      //     southwest: {
+      //       latitude: bounds.getSouthWest().lat() - 0.005,
+      //       longitude: bounds.getSouthWest().lng() - 0.005
+      //     }
+      //   };
 
-        $scope.map.markers = markers;
+      //   // Set the map's markers
+      //   $scope.map.markers = markers;
+      // });
+      // 
+      
+      Location.getLocation(function(err, location) {
+        if (err) { 
+          console.log(err); 
+        } else {
+          $scope.map.center = angular.copy(location);
+          $scope.map.markers = [location];
+          $scope.mapLoaded = true;
+          $rootScope.$$phase || $rootScope.$apply();
+        }
       });
+
     });
 
 
---
+++ client/components/location/location.service.js
@@ -0,0 +1,37 @@
+'use strict';
+
+angular.module('lostAndFoundApp')
+  .service('Location', function () {
+  	var location = null;
+
+  	return {
+
+      /**
+       * Get the users location
+       * 
+       * @param  {Function} callback [description]
+       * @return {[type]}            [description]
+       */
+  		getLocation: function(callback) {
+        if (location) {
+          callback(null, location);
+        } else {
+          if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+              location = {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+              };
+              callback(null, location);
+            }, function(err) {
+              callback(err)
+            });
+          } else {
+            callback("/* geolocation IS NOT available */");
+          }          
+        }
+  		}
+      
+  	};
+
+  });
